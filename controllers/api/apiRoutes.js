9f9bbadc46cf63a68fff4554d6912df090620ef6
steveSnyder <74982034+stevesnyder@users.noreply.github.com>
2021-06-07 20:39:37 -0400

    fixed routes

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Workout = require('../models/workout.js');
+const Workout = require('../../models/Workout');
 
 router.post('/workouts', (req, res) => {
   Workout.create({})
